f94aa5f29e38820445b71a079d6a652dba782a8b
TejasJagadale <139218507+TejasJagadale@users.noreply.github.com>
2025-08-18 16:32:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ const allowedOrigins = [
  "https://todaytalks.in",
  "http://localhost:3000"
 ];
-console.log("hi this is frontend");
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -62,6 +61,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
