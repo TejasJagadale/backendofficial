aff699559de10275c4eafccdc0e9c00d8956f9f7
TejasJagadale <139218507+TejasJagadale@users.noreply.github.com>
2025-08-20 10:34:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const connectDB = require("./config/db");
 const contentRoutes = require("./routes/contentRoutes");
 const s3Routes = require("./routes/s3Routes");
 const commentRoutes = require("./routes/commentRoutes");
-
-
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -41,6 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/s3", s3Routes);
 app.use("/api/contents", contentRoutes);
 app.use("/api/comments", commentRoutes);
@@ -61,7 +61,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 
 
 
