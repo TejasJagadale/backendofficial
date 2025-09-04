ea3bedbc45c71291c01a760234ff31a0b0ce2289
TejasJagadale <139218507+TejasJagadale@users.noreply.github.com>
2025-09-04 14:50:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,8 +67,13 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server running on port 3000");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
